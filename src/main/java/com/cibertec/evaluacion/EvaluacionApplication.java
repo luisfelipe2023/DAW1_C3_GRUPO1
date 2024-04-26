43bc388c34bfba639e7ed2ac1ffbebc3ef71d596
luisfelipe2023 <138238233+luisfelipe2023@users.noreply.github.com>
2024-04-26 09:58:32 -0500

    Add files via upload
    Estructura del Proyecto

---
+++ src/main/java/com/cibertec/evaluacion/EvaluacionApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.evaluacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluacionApplication.class, args);
+	}
+
+}
