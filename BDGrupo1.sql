3361551407f2ab74419ad4557e02a3318b2b1902
AdrianLlontop <rizzaga1234@gmail.com>
2024-04-26 09:49:10 -0500

    bd agregada

---
+++ BDGrupo1.sql
@@ -0,0 +1,52 @@
+Create Database BDGrupo1
+
+use BDGrupo1
+
+CREATE TABLE usuario (
+	idusuario INT auto_increment NOT NULL,
+	nomusuario varchar(100) NULL,
+	email varchar(200) NULL,
+	password varchar(300) NULL,
+	nombres varchar(100) NULL,
+	apellidos varchar(100) NULL,
+	activo BOOL NULL,
+	CONSTRAINT users_pk PRIMARY KEY (idusuario)
+);
+
+
+CREATE TABLE usuario_rol (
+    idusuario INT NOT NULL,
+    idrol INT NOT NULL,
+    PRIMARY KEY (idusuario, idrol),
+    FOREIGN KEY (idusuario) REFERENCES usuario(idusuario),
+    FOREIGN KEY (idrol) REFERENCES rol(idrol)
+);
+
+
+CREATE TABLE usuario_rol (
+	idusuario INT NOT NULL,
+	idrol INT NOT NULL,
+	CONSTRAINT user_role_pk PRIMARY KEY (idusuario, idrol),
+	CONSTRAINT user_role_FK FOREIGN KEY (idusuario) REFERENCES bdventas.usuario(idusuario),
+	CONSTRAINT user_role_FK_1 FOREIGN KEY (idrol) REFERENCES bdventas.rol(idrol)
+);
+
+CREATE TABLE Autor
+(
+ IdAutor 		INT NOT NULL AUTO_INCREMENT,
+ NomAutor		VARCHAR(50) NOT NULL,
+ ApeAutor 		VARCHAR(50) NOT NULL,
+ FechNacAutor	DATE NOT NULL,
+ PRIMARY KEY (IdAutor)
+);
+
+CREATE TABLE Publicacion
+(
+ IdPublicacion 		INT NOT NULL AUTO_INCREMENT,
+ Titulo 			VARCHAR(250) NOT NULL,
+ Resumen 			VARCHAR(250) NOT NULL,
+ FechPublicacion	DATE NOT NULL,
+ IdAutor 			INT NOT NULL,
+ PRIMARY KEY (IdPublicacion),
+ FOREIGN KEY (IdAutor) REFERENCES Autor(IdAutor)
+ );
